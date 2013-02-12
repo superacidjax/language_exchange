9dea3adf44d4ba230b21e166e39d914e4a5b58f5
Brian Dear <superacidjax@me.com>
2013-02-11 23:13:25 -0500

    Adds serialization option for languages and days avail

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
   attr_accessible :age, :bio, :city, :country, :days_available, :email, :facebook, :first_name, :gender, :imessage, :lang_learning, :lang_spoken, :last_name, :msn, :skype
+  serialize :lang_learning, :lang_spoken, :days_available
 end
