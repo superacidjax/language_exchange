bca4a9156a70bb375d03eeb19b0ca00f6949c6c3
Brian Dear <superacidjax@me.com>
2013-02-17 01:02:29 -0500

    configures fog for S3 uploads

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,5 +1,6 @@
 CarrierWave.configure do |config|
   if Rails.env.staging? || Rails.env.production?
+    config.storage = :fog
     config.fog_credentials = {
       provider:             'AWS',
       aws_access_key_id:     ENV['S3_ACCESS_KEY_ID'],
