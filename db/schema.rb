d67a295503af265ec8f371df46bf1aed12a9680c
Brian Dear <superacidjax@me.com>
2013-02-17 00:02:16 -0500

    Updates schema for user photo

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130214163254) do
+ActiveRecord::Schema.define(:version => 20130217001924) do
 
   create_table "users", :force => true do |t|
     t.string   "first_name"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(:version => 20130214163254) do
     t.string   "remember_me_token"
     t.datetime "remember_me_token_expires_at"
     t.date     "birthday"
+    t.string   "photo"
   end
 
   add_index "users", ["remember_me_token"], :name => "index_users_on_remember_me_token"
