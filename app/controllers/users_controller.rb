239da7c3007cfe38098dc850d29e46558b3dc967
Brian Dear <superacidjax@me.com>
2013-02-17 00:06:16 -0500

    Ensures only correct user can access settings

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,15 @@
 class UsersController < ApplicationController
 
+  before_filter :require_login, only: [:update, :destroy]
+  before_filter :the_right_user,  only: [:edit, :update, :destroy]
   expose(:user)
   expose(:users)
 
 
   def create
     if user.save
-      redirect_to(user)
+      auto_login(user)
+      redirect_to edit_user_path(user), :notice => "Your account has been created!"
     else
       render :new
     end
@@ -19,5 +22,18 @@ class UsersController < ApplicationController
       render :edit
     end
   end
+
+  private
+
+    def the_right_user
+      if current_user != user
+        redirect_to root_path, alert: "You aren't allowed to edit someone else's profile!"
+      else
+      end
+    end
+
+    # def admin_user
+    #   redirect_to root_path unless current_user.admin?
+    # end
 
 end
