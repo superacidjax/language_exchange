e73b4167057df006496cc57f837597bf65a9933f
Brian Dear <superacidjax@me.com>
2013-02-17 00:06:46 -0500

    Creates first name and last initial view helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+
+  def user_name(u)
+    first_name = u.first_name
+    last_name = truncate(u.last_name, length: 2, omission: '.')
+    return "#{first_name} #{last_name}"
+  end
 end
