8d48e684b910537cbb15a9717c9adf270c96a6d4
Brian Dear <superacidjax@me.com>
2013-04-05 19:06:19 -0600

    configures mail for prod and dev envs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,17 @@ LanguageExchange::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => "heroku.com",
+    :address => "smtp.sendgrid.net",
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
 
